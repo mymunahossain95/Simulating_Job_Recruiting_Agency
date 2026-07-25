6134b82fee8e926e09e89de60d7d298ed85d1e69
Nazila Islam <2411782@iub.edu.bd>
2026-07-25 22:38:08 +0600

    CareerCounselor Goal1 git-push

--- src/main/java/Controller/CareerCounselorDashboardController.java
+++ src/main/java/Controller/CareerCounselorDashboardController.java
@@ -1,4 +1,35 @@
 package Controller;
 
 public class CareerCounselorDashboardController {
+    @javafx.fxml.FXML
+    public void handleCareerGuidanceButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleTrainingRecommendationsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleJobMatchingButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAssistanceRequestsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleCounselingSessionsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleCandidateTrackingButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleInterviewGuidanceButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleResumeReviewButton(ActionEvent actionEvent) {
+    }
 }

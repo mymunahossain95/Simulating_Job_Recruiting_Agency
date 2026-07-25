4d985f3e86f338e5944e3d2b0e662d963036f249
Victus <2411776@iub.edu.bd>
2026-07-25 22:34:57 +0600

    controller updated

--- src/main/java/Controller/ManageUserAccountsController.java
+++ src/main/java/Controller/ManageUserAccountsController.java
@@ -1,4 +1,42 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ManageUserAccountsController {
+    @javafx.fxml.FXML
+    private TableColumn nameColumn;
+    @javafx.fxml.FXML
+    private TableColumn roleColumn;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private ComboBox roleComboBox;
+    @javafx.fxml.FXML
+    private TableView userTable;
+    @javafx.fxml.FXML
+    private TableColumn userIdColum;
+    @javafx.fxml.FXML
+    private TextField searchTextField;
+    @javafx.fxml.FXML
+    private TableColumn emailColumn;
+
+    @javafx.fxml.FXML
+    public void onDelete(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onCreate(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onSearch(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onUpdate(ActionEvent actionEvent) {
+    }
 }
---
+++ src/main/java/Controller/SystemSettingsController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class SystemSettingsController {
+    @javafx.fxml.FXML
+    private TextField settingValueField;
+
+    @javafx.fxml.FXML
+    public void onSaveSettings(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void settingTypeCombo(ActionEvent actionEvent) {
+    }
+}

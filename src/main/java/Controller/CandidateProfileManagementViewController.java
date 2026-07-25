fe65ba095f16316e58e5a40d9148e3c9aee9f066
mymunahossain95 <2412444@iub.edu.bd>
2026-07-25 23:48:00 +0600

    updated again

--- src/main/java/Controller/CandidateProfileManagementViewController.java
+++ src/main/java/Controller/CandidateProfileManagementViewController.java
@@ -1,4 +1,24 @@
 package Controller;
 
 public class CandidateProfileManagementViewController {
+    @javafx.fxml.FXML
+    private TextField phoneTF;
+    @javafx.fxml.FXML
+    private TextField nameTF;
+    @javafx.fxml.FXML
+    private TextField emailTF;
+    @javafx.fxml.FXML
+    private TextField skillsTF;
+    @javafx.fxml.FXML
+    private TextField educatioTF;
+    @javafx.fxml.FXML
+    private TextField experienceTF;
+
+    @javafx.fxml.FXML
+    public void editOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveOA(ActionEvent actionEvent) {
+    }
 }

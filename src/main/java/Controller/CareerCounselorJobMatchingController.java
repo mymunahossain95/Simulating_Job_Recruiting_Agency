5f99a9a1d3c6fcddc96de26971c34504a658d988
Nazila Islam <2411782@iub.edu.bd>
2026-07-26 03:07:19 +0600

    carrercounselorgoal4jobmatching

--- src/main/java/Controller/CareerCounselorJobMatchingController.java
+++ src/main/java/Controller/CareerCounselorJobMatchingController.java
@@ -1,4 +1,44 @@
 package Controller;
 
 public class CareerCounselorJobMatchingController {
+    @javafx.fxml.FXML
+    private Label jobMatchingCandidateListLabel;
+    @javafx.fxml.FXML
+    private TableColumn jobMatchingcandidListCandidateIDCol1;
+    @javafx.fxml.FXML
+    private Label jobMatchingvailableJobsLabel;
+    @javafx.fxml.FXML
+    private TableColumn jobMatchingJobTitleCol1;
+    @javafx.fxml.FXML
+    private TableColumn jobMatchingLocationCol4;
+    @javafx.fxml.FXML
+    private TableColumn jobMatchingcandidListCandidateNameCol2;
+    @javafx.fxml.FXML
+    private TableView candidateListTableView;
+    @javafx.fxml.FXML
+    private Label jobMatchingLabel;
+    @javafx.fxml.FXML
+    private TableColumn jobMatchingRequiredSkillsCol3;
+    @javafx.fxml.FXML
+    private TableView jobMatchingAvailableJobsTableView;
+    @javafx.fxml.FXML
+    private TableColumn jobMatchingCompanyNameCol2;
+    @javafx.fxml.FXML
+    private TableColumn jobMatchingcandidListSkillsCol3;
+
+    @javafx.fxml.FXML
+    public void handleJobMatchinRecommendJobOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleJobMatchinAnalyzeMatchOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleJobMatchinSendRecommendationOA(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleJobMatchingBackOA(ActionEvent actionEvent) {
+    }
 }

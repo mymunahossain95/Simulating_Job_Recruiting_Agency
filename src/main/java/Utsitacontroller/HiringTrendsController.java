c873e36d91ae9ab852aa1fe651e6b50fb8f918bb
SoftwareGirly <nusratjahanutsita@gmail.com>
2026-07-26 05:04:01 +0600

    Controller and FXML updates For all User-7 Goals

---
+++ src/main/java/Utsitacontroller/HiringTrendsController.java
@@ -0,0 +1,45 @@
+package Utsitacontroller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class HiringTrendsController {
+    @javafx.fxml.FXML
+    private TableColumn indusTryTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn hiringTrendTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn employerIdTableColumn;
+    @javafx.fxml.FXML
+    private DatePicker endDateDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn companyNameTableColumn;
+    @javafx.fxml.FXML
+    private TextField searchCompanyTextField;
+    @javafx.fxml.FXML
+    private ComboBox industRyCombobox;
+    @javafx.fxml.FXML
+    private TableView hiringTrendTableView;
+    @javafx.fxml.FXML
+    private TableColumn vacanciesPostedTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn employeesHiredTableColumn;
+    @javafx.fxml.FXML
+    private DatePicker startDateDatePicker;
+
+    @javafx.fxml.FXML
+    public void onBack(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onApplyFilter(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onRefresh(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void onGenerateReport(ActionEvent actionEvent) {
+    }
+}

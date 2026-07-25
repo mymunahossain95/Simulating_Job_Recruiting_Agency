f018a6bf016bfb27ae24dd0ea0c592392ca44fa9
rohan <2411858@iub.edu.bd>
2026-07-26 05:11:55 +0600

    all fxml and controller created rhn

---
+++ src/main/java/Controller/GenerateSkillScoreController.java
@@ -0,0 +1,49 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class GenerateSkillScoreController {
+    @javafx.fxml.FXML
+    private Label totalScoreLabel;
+    @javafx.fxml.FXML
+    private Label gradeLabel;
+    @javafx.fxml.FXML
+    private TableColumn percentageColumn;
+    @javafx.fxml.FXML
+    private TableColumn scoreColumn;
+    @javafx.fxml.FXML
+    private ComboBox candidateComboBox;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+    @javafx.fxml.FXML
+    private Button exportBtnOnAction;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableView scoreTable;
+    @javafx.fxml.FXML
+    private TableColumn candidateColumn;
+    @javafx.fxml.FXML
+    private TableColumn gradeColumn;
+    @javafx.fxml.FXML
+    private Label percentageLabel;
+
+    @javafx.fxml.FXML
+    public void generateScoreBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void resetBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboardBtnOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/Controller/UpdateAssessmentCriteriaController.java
@@ -0,0 +1,37 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class UpdateAssessmentCriteriaController {
+    @javafx.fxml.FXML
+    private TableColumn updateDateColumn;
+    @javafx.fxml.FXML
+    private ComboBox categoryComboBox;
+    @javafx.fxml.FXML
+    private Label titleLabel;
+    @javafx.fxml.FXML
+    private TableColumn weightageColumn;
+    @javafx.fxml.FXML
+    private TableView criteriaTable;
+    @javafx.fxml.FXML
+    private TableColumn categoryColumn;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TextField weightageField;
+    @javafx.fxml.FXML
+    private Button clearBtnOnAction1;
+
+    @javafx.fxml.FXML
+    public void updateBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboardBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearBtnOnAction(ActionEvent actionEvent) {
+    }
+}

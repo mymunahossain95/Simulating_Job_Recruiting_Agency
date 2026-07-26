63c84915eb46e12d830ad2338df5cffb6d4126ba
rohan <2411858@iub.edu.bd>
2026-07-27 02:02:36 +0600

    all fxml and controller created rhn for user 6(int. coordinator) #2

--- src/main/java/Controller/EvaluateAssessmentController.java
+++ src/main/java/Controller/EvaluateAssessmentController.java
@@ -1,4 +1,45 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class EvaluateAssessmentController {
+    @javafx.fxml.FXML
+    private TableView submissionTableView;
+    @javafx.fxml.FXML
+    private TableColumn assessmentColumnEvo;
+    @javafx.fxml.FXML
+    private TableView evaluationTable;
+    @javafx.fxml.FXML
+    private TextField answerAreaTF;
+    @javafx.fxml.FXML
+    private TextField scoreFieldTF;
+    @javafx.fxml.FXML
+    private TableColumn candidateColumnforEvo;
+    @javafx.fxml.FXML
+    private TextField remarksAreaTF;
+    @javafx.fxml.FXML
+    private TableColumn assessmentColumn;
+    @javafx.fxml.FXML
+    private TableColumn candidateColumn;
+    @javafx.fxml.FXML
+    private TableColumn scoreColumnEvo;
+    @javafx.fxml.FXML
+    private TableColumn submissionDateColumn;
+    @javafx.fxml.FXML
+    private TableColumn evaluationDateColumnEvo;
+
+    @javafx.fxml.FXML
+    public void dashboardBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearBtnOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void submitEvaluationBtnOnAction(ActionEvent actionEvent) {
+    }
 }

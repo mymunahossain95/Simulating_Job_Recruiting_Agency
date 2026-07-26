e778032a16d3bc84ea0d55db4d2393e4278cada9
SoftwareGirly <nusratjahanutsita@gmail.com>
2026-07-27 01:22:55 +0600

    Controller and FXML updates For all User-8 Goals

---
+++ src/main/java/Utsitacontroller/TrainingScheduleController.java
@@ -0,0 +1,55 @@
+package Utsitacontroller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class TrainingScheduleController {
+    @javafx.fxml.FXML
+    private TableColumn programNameTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn sessionIdTableColumn;
+    @javafx.fxml.FXML
+    private DatePicker sessionDateDatePicker;
+    @javafx.fxml.FXML
+    private TextField endTimeTextField;
+    @javafx.fxml.FXML
+    private TableColumn TiMeTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn daTeTableColumn;
+    @javafx.fxml.FXML
+    private TextField venueTextField;
+    @javafx.fxml.FXML
+    private TableColumn venueTableColumn;
+    @javafx.fxml.FXML
+    private TextField perticipantLimitTextField;
+    @javafx.fxml.FXML
+    private TextArea sessionNotesTextArea;
+    @javafx.fxml.FXML
+    private TableView trainingSessionTableView;
+    @javafx.fxml.FXML
+    private TableColumn traInerTableColumn;
+    @javafx.fxml.FXML
+    private ComboBox trainerComboBox;
+    @javafx.fxml.FXML
+    private TableColumn availableSeatsTableColumn;
+    @javafx.fxml.FXML
+    private ComboBox trainingProgramComboBox;
+    @javafx.fxml.FXML
+    private TextField startTimeTextField;
+
+    @javafx.fxml.FXML
+    public void createScheduleButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveButton(ActionEvent actionEvent) {
+    }
+}

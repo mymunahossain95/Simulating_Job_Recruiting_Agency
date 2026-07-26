22c583380a5c0b34ce5b1add03ab06bdbf5da4df
Nazila Islam <2411782@iub.edu.bd>
2026-07-26 23:44:39 +0600

    documentverfiergoal1,2,3,4,5pushed

---
+++ src/main/java/Controller/DocumentVerificationEducationVerificationController.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class DocumentVerificationEducationVerificationController {
+    @javafx.fxml.FXML
+    private Label educationVerificationQualificationStatusLabel;
+    @javafx.fxml.FXML
+    private TableView educationVerificationTableView;
+    @javafx.fxml.FXML
+    private TextField educationVerificatiomRemarksTextField;
+    @javafx.fxml.FXML
+    private TableColumn educationVerificationDegreeCertificatesCol2;
+
+    @javafx.fxml.FXML
+    public void educationVerificationVerifyQualificationButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void educationVerificationViewCertificateButtonOnAction(ActionEvent actionEvent) {
+    }
+}

ebc9011e345d206584d9b4799cf296cd487b8b50
Nazila Islam <2411782@iub.edu.bd>
2026-08-07 03:50:58 +0600

    pull

--- src/main/java/User/DocumentVerifier.java
+++ src/main/java/User/DocumentVerifier.java
@@ -1,4 +1,34 @@
 package User;
 
-public class DocumentVerifier {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class DocumentVerifier extends User implements Serializable {
+
+    private final String verifierId;
+
+    public DocumentVerifier(String userId, String verifierId, String fullName,
+            String email, String password, LocalDate dateOfBirth, String phone,
+            String address, String status) {
+
+        super(userId, fullName, email, password, dateOfBirth, phone, address, status
+        );
+
+        this.verifierId = verifierId;
+    }
+
+    public String getVerifierId() {
+        return verifierId;
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentVerifier{" +
+                "verifierId='" + verifierId + '\'' +
+                ", userId='" + getUserId() + '\'' +
+                ", fullName='" + getFullName() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", status='" + getStatus() + '\'' +
+                '}';
+    }
 }
